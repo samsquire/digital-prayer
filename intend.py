ca6c0c51072745cdf5b22e64ee6d8e010fdf528c
Sam Squire <sam@samsquire.com>
2020-05-14 00:50:16 +0100

    Sam digital prayer

---
+++ intend.py
@@ -0,0 +1,23 @@
+import time
+
+class Gauge():
+	def __init__(self):
+		self.marks = 0
+		self.last_time = time.time()
+		self.rate = ""
+	
+	def mark(self):
+		self.marks = self.marks + 1
+		if self.marks == 10000:
+			self.marks = 0
+			new_time = time.time()
+			self.rate = 1 / ((new_time - self.last_time) / 10000)
+			self.last_time = new_time
+            
+
+g = Gauge()
+
+while True:
+	print("{} {}".format(g.rate, "I pray for universal basic income."))
+	g.mark()
+
